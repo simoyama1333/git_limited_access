665d9b5db5a32687616c60794aa8fc0786c801c5
niizaki <simoyama1333@yahoo.co.jp>
2019-11-18 22:53:28 +0900

    gitapi ok

--- golang/git_api.go
+++ golang/git_api.go
@@ -7,8 +7,9 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"strings"
+	"encoding/base64"
 )
-	
+//by https://mholt.github.io/json-to-go/
 type GitJson struct {
 	Name        string      `json:"name"`
 	Path        string      `json:"path"`
@@ -25,6 +26,24 @@ type GitJson struct {
 		HTML string `json:"html"`
 	} `json:"_links"`
 }
+type GitFIle struct {
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Sha         string `json:"sha"`
+	Size        int    `json:"size"`
+	URL         string `json:"url"`
+	HTMLURL     string `json:"html_url"`
+	GitURL      string `json:"git_url"`
+	DownloadURL string `json:"download_url"`
+	Type        string `json:"type"`
+	Content     string `json:"content"`
+	Encoding    string `json:"encoding"`
+	Links       struct {
+		Self string `json:"self"`
+		Git  string `json:"git"`
+		HTML string `json:"html"`
+	} `json:"_links"`
+}
 type FileOrDir struct {
 	Name string
 	Path string
@@ -33,7 +52,7 @@ type FileOrDir struct {
 }
 
 var token string
-
+var repourl string
 
 func main() {
     flag.Parse()
@@ -45,12 +64,18 @@ func main() {
 	token = "ab242b969e1b170b4966267ff24fe9cf5b538596"
 
 	var username string = "simoyama1333"
-	var giturl string = "https://api.github.com/repos/" + username + "/" + reponame + "/contents?access_token=" + token
-
+	repourl = "https://api.github.com/repos/" + username + "/" + reponame + "/"
+	/*
+	var gitFirtUrl := repourl + "contents?access_token=" + token
 	fmt.Println("Now crawling")
-	contents := GetContentsJson(giturl)
+	contents := GetContentsJson(gitFirstUrl)
 	data := ContentsToDataRecursively(contents)
 	fmt.Println(data)
+	*/
+	a := GetFileAndDecode("README.md")
+	fmt.Println(a)
+
+
 }
 //gitのファイルを再帰的に取得
 func ContentsToDataRecursively(contents []GitJson) []FileOrDir{
@@ -94,4 +119,39 @@ func GetContentsJson(giturl string) []GitJson {
 		panic(err)
 	}
 	return data
+}
+
+
+//gitのファイルをAPIから取得する場合、Base64デコードが必要となる
+func GetFileAndDecode(path string) string{
+	giturl := repourl + "contents/" + path + "?access_token=" + token
+	fmt.Println(giturl)
+	resp, err := http.Get(giturl)
+	if err != nil {
+		panic(err)
+	}
+  
+	defer resp.Body.Close()
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonBytes := ([]byte)(byteArray)
+	
+	var data GitFIle
+
+    if err := json.Unmarshal(jsonBytes, &data); err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+
+	encoded64 := strings.Replace(data.Content, "\n", "", -1) 
+	fmt.Println(encoded64)
+	retstr, err := base64.StdEncoding.DecodeString(encoded64)
+	if err != nil {
+			fmt.Println("nocontent :", err)
+			return ""
+	}
+	return string(retstr)
 }
--- golang/main.go
+++ golang/main.go
@@ -1,100 +1,159 @@
 package main
 
 import (
-    "context"
-    "log"
-    "time"
-
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+    "flag"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"strings"
+	"encoding/base64"
 )
-
-func insertBsonD(col *mongo.Collection) error {
-    bsonD := bson.D{
-        {"str1", "abc"},
-        {"num1", 1},
-        {"str2", "xyz"},
-        {"num2", bson.A{2, 3, 4}},
-        {"subdoc", bson.D{{"str", "subdoc"}, {"num", 987}}},
-        {"date", time.Now()},
-    }
-    _, err := col.InsertOne(context.Background(), bsonD)
-    return err
+//by https://mholt.github.io/json-to-go/
+type GitJson struct {
+	Name        string      `json:"name"`
+	Path        string      `json:"path"`
+	Sha         string      `json:"sha"`
+	Size        int         `json:"size"`
+	URL         string      `json:"url"`
+	HTMLURL     string      `json:"html_url"`
+	GitURL      string      `json:"git_url"`
+	DownloadURL interface{} `json:"download_url"`
+	Type        string      `json:"type"`
+	Links       struct {
+		Self string `json:"self"`
+		Git  string `json:"git"`
+		HTML string `json:"html"`
+	} `json:"_links"`
+}
+type GitFIle struct {
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Sha         string `json:"sha"`
+	Size        int    `json:"size"`
+	URL         string `json:"url"`
+	HTMLURL     string `json:"html_url"`
+	GitURL      string `json:"git_url"`
+	DownloadURL string `json:"download_url"`
+	Type        string `json:"type"`
+	Content     string `json:"content"`
+	Encoding    string `json:"encoding"`
+	Links       struct {
+		Self string `json:"self"`
+		Git  string `json:"git"`
+		HTML string `json:"html"`
+	} `json:"_links"`
 }
 
-func insertBsonM(col *mongo.Collection) error {
-    bsonM := bson.M{
-        "str1": "efg",
-        "num1": 11,
-        "str2": "opq",
-        "num2": bson.A{12, 13, 14},
-        "subdoc": bson.M{"str": "subdoc", "num": 987},
-        "date": time.Now(),
-    }
-    for i := 0; i < 10; i++ {
-        _, err := col.InsertOne(context.Background(), bsonM)
-        if err != nil {
-            return err
-        }
-    }
-    return nil
+
+type FileOrDir struct {
+	Name string
+	Path string
+	TypeFile bool
+	Files []FileOrDir
 }
 
-type myType struct {
-    Str1 string
-    Num1 int
-    Str2 string
-    Num2 []int
-    Subdoc struct {
-        Str string
-        Num int
-    }
-    Date time.Time
-}
-
-func insertStruct(col *mongo.Collection) error {
-    doc := myType{
-        "hij",
-        21,
-        "rst",
-        []int{22, 23, 24},
-        struct {
-            Str string
-            Num int
-        }{"subdoc", 987},
-        time.Now(),
-    }
-    _, err := col.InsertOne(context.Background(), doc)
-    return err
-}
-
-func mainMain() error {
-    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:mongo@mongodb:27017"))
-    if err != nil {
-        return err
-    }
-    if err = client.Connect(context.Background()); err != nil {
-        return err
-    }
-    defer client.Disconnect(context.Background())
-
-    col := client.Database("test").Collection("col")
-    if err = insertBsonD(col); err != nil {
-        return err
-    }
-    if err = insertBsonM(col); err != nil {
-        return err
-    }
-    if err = insertStruct(col); err != nil {
-        return err
-    }
-    return nil
-}
+var token string
+var repourl string
 
 func main() {
-    if err := mainMain(); err != nil {
-        log.Fatal(err)
-    }
-    log.Println("normal end.")
+    flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Println("Put repository name")
+		return 
+	}
+	reponame := flag.Args()[0]
+	token = "ab242b969e1b170b4966267ff24fe9cf5b538596"
+
+	var username string = "simoyama1333"
+	repourl = "https://api.github.com/repos/" + username + "/" + reponame + "/"
+	/*
+	var gitFirtUrl := repourl + "contents?access_token=" + token
+	fmt.Println("Now crawling")
+	contents := GetContentsJson(gitFirstUrl)
+	data := ContentsToDataRecursively(contents)
+	fmt.Println(data)
+	*/
+	a := GetFileAndDecode("README.md")
+	fmt.Println(a)
+
+
+}
+//gitのファイルを再帰的に取得
+func ContentsToDataRecursively(contents []GitJson) []FileOrDir{
+	var data []FileOrDir
+	for _,item := range contents{
+		info := FileOrDir{}
+		info.Name = item.Name
+		info.Path = item.Path
+		info.Files = []FileOrDir{}
+		if item.Type == "dir"{
+			info.TypeFile = false
+			url := item.URL
+			url = strings.Replace(url, "ref=master", "", 1) + "access_token=" + token
+			contents := GetContentsJson(url)
+			info.Files = ContentsToDataRecursively(contents) 
+		}else{
+			info.TypeFile = true
+		}
+		data = append(data,info)
+	}
+	return data
+}
+
+func GetContentsJson(giturl string) []GitJson {
+	resp, err := http.Get(giturl)
+	if err != nil {
+		panic(err)
+	}
+  
+	defer resp.Body.Close()
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonBytes := ([]byte)(byteArray)
+	
+	var data []GitJson
+
+    if err := json.Unmarshal(jsonBytes, &data); err != nil {
+		panic(err)
+	}
+	return data
+}
+
+
+//gitのファイルをAPIから取得する場合、Base64デコードが必要となる
+func GetFileAndDecode(path string) string{
+	giturl := repourl + "contents/" + path + "?access_token=" + token
+	fmt.Println(giturl)
+	resp, err := http.Get(giturl)
+	if err != nil {
+		panic(err)
+	}
+  
+	defer resp.Body.Close()
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonBytes := ([]byte)(byteArray)
+	
+	var data GitFIle
+
+    if err := json.Unmarshal(jsonBytes, &data); err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+
+	encoded64 := strings.Replace(data.Content, "\n", "", -1) 
+	fmt.Println(encoded64)
+	retstr, err := base64.StdEncoding.DecodeString(encoded64)
+	if err != nil {
+			fmt.Println("nocontent :", err)
+			return ""
+	}
+	return string(retstr)
 }
